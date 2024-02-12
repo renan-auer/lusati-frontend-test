13a2c81a4b64cbbcc1401e19ef27676a508fc6d5
Renan <renan.auer@gmail.com>
2024-02-12 13:22:56 -0300

    Implementação de loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { LoadingService } from './services/loading.service';
 
 @Component({
     selector: 'app-root',
@@ -7,7 +8,7 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
 
-    constructor(private primengConfig: PrimeNGConfig) { }
+    constructor(private primengConfig: PrimeNGConfig, public loadingService: LoadingService) { }
 
     ngOnInit() {
         this.primengConfig.ripple = true;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
-import { PathLocationStrategy, LocationStrategy } from '@angular/common';
+import { PathLocationStrategy, LocationStrategy, CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { ContatoService } from './services/contato.service';
 import { GrupoService } from './services/grupo.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingInterceptor } from './interceptor/loader.interceptor';
+import { LoadingService } from './services/loading.service';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @NgModule({
     declarations: [AppComponent, NotfoundComponent],
-    imports: [AppRoutingModule, AppLayoutModule],
+    imports: [AppRoutingModule, AppLayoutModule, CommonModule, ProgressSpinnerModule ],
     providers: [
         { provide: LocationStrategy, useClass: PathLocationStrategy },
         ContatoService,
+        LoadingService,
+        { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
         GrupoService
     ],
     bootstrap: [AppComponent],
